0e5f54a96d52322c172d86aa67968e3f45dcf833
Milena <milenaalmeidadev@hotmail.com>
2021-06-02 15:49:45 -0400

    Page Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,85 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <img src="/logo.svg" alt="logo" />
+      </header>
+
+      <section>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.info}>
+          <span>
+            <FiCalendar />
+            15 Junho
+          </span>
+          <span>
+            <FiUser />
+            Milena Almeida
+          </span>
+        </div>
+      </section>
+
+      <section>
+        <h1>Criando um app CRA do zero</h1>
+        <p>
+          Tudo sobre como criar a sua primeira aplicação utilizando Create React
+          App
+        </p>
+        <div className={styles.info}>
+          <span>
+            <FiCalendar />
+            20 Junho
+          </span>
+          <span>
+            <FiUser />
+            Milena Almeida
+          </span>
+        </div>
+      </section>
+
+      <section>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div className={styles.info}>
+          <span>
+            <FiCalendar />
+            15 Junho
+          </span>
+          <span>
+            <FiUser />
+            Milena Almeida
+          </span>
+        </div>
+      </section>
+
+      <section>
+        <h1>Criando um app CRA do zero</h1>
+        <p>
+          Tudo sobre como criar a sua primeira aplicação utilizando Create React
+          App
+        </p>
+        <div className={styles.info}>
+          <span>
+            <FiCalendar />
+            20 Junho
+          </span>
+          <span>
+            <FiUser />
+            Milena Almeida
+          </span>
+        </div>
+      </section>
+
+      <Link href="/">
+        <a className={styles.link}>Carregar mais posts</a>
+      </Link>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
